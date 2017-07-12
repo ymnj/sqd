7b69fdc36c0e3b9ba2823d2022f3448603f447f1
Tom Hung <tom@tomhung.ca>
2017-07-11 22:24:53 -0700

    Add initial main header, nav, banner. Include assets in functions.php.

--- functions.php
+++ functions.php
@@ -1,1 +1,21 @@
+<?php 
+
+function main_assets(){
+  
+  wp_register_style('normalize', get_template_directory_uri() . '/assets/css/normalize.css' );  
+  wp_register_style('pure', get_template_directory_uri() . '/assets/css/pure.css' );
+  wp_register_style('pure-responsive', get_template_directory_uri() . '/assets/css/pure-responsive.css', array('pure'))  ;
+  wp_register_style('main-styles', get_stylesheet_uri(), array() );
+
+  wp_register_script('main-js', get_template_directory_uri() . '/assets/js/main-js.js');
+
+  wp_enqueue_style('normalize');
+  wp_enqueue_style('pure');
+  wp_enqueue_style('pure-responsive');
+  wp_enqueue_style('main-styles');
+  wp_enqueue_script('main-js');
+}
+
+add_action( 'wp_enqueue_scripts', 'main_assets' );
+
 
--- index.php
+++ index.php
@@ -2,9 +2,40 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>SQD.</title>
+  <title>SQD</title>
+  <?php wp_head() ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-  Hello world
+
+  <header>
+    
+    <h1>SQD</h1>
+
+    <nav>
+      <ul>
+        <li><a href="index.html">About Us</a></li>
+        <li><a href="/about/">Store</a></li>
+        <li><a href="/blog/">News</a></li>
+        <li><a href="">Contacts</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <div class="top-banner">
+    <div class="join-cta">Join Now</div>
+  </div>
+
+  <div class="pure-g">
+    <div class="pure-u-1 pure-u-md-1-3"> ... </div>
+    <div class="pure-u-1 pure-u-md-1-3"> ... </div>
+    <div class="pure-u-1 pure-u-md-1-3"> ... </div>
+</div>
+ 
+
+  
+
+
+  <?php wp_footer() ?>
 </body>
 </html>
