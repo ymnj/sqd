b11ceb1bc7e7cc1ed68052dd00e1e11e1af04fe5
Tom Hung <tom@tomhung.ca>
2017-07-26 23:44:00 -0700

    Add front page blog news.

--- index.php
+++ index.php
@@ -36,17 +36,7 @@
 
   <?php get_template_part('/sections/section', 'testimonials'); ?>
 
-  <div class="container">
-     <div class="latest-blogs pure-g">
-       <div class="pure-u-1">
-         <h1 class="header"><span class="bold-head">See What's New</span> on our blog</h1>
-       </div>
-       <div class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4"">1</div>
-       <div class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4"">2</div>
-       <div class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4"">3</div>
-       <div class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4"">4</div>
-     </div>
-  </div>
+  <?php get_template_part('/sections/section', 'blog'); ?>
 
 
   <?php wp_footer() ?>
---
+++ sections/section-blog.php
@@ -0,0 +1,18 @@
+<div class="container">
+   <div class="latest-blogs pure-g">
+     <div class="pure-u-1">
+       <h1 class="header"><span class="bold-head">See What's New</span> on our blog</h1>
+     </div>
+     <article class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4"">
+       <h1 class="header"><span class="bold-head">May 03</span></h1>
+       <p class="blog-meta">Posted by <span class="blog-author">admin</span><span class="blog-comments">0 comments</span></p>
+       <img src="<?php echo get_template_directory_uri() . '/assets/images/blog1.jpg' ?>" alt="" class="blog-image">
+       <h2 class="blog-title">New styles of 2017</h2>
+       <p class="blog-excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium animi accusamus numquam, odio velit explicabo, cupiditate repellendus ut similique nihil architecto libero quam quia iusto et voluptatibus itaque modi est!</p>
+       <div class="blog-more-btn">Read More</div>
+     </article>
+     <article class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4"">2</article>
+     <article class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4"">3</article>
+     <article class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4"">4</article>
+   </div>
+</div>
