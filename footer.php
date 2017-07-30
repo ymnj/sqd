151d2cc5d8b8480f711c1fd614ee0771a56d287e
Tom Hung <tom@tomhung.ca>
2017-07-29 22:27:24 -0700

    Add footer styles.

--- footer.php
+++ footer.php
@@ -5,13 +5,33 @@
     </section>
     <section class="widget-categories pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
       <h2>Categories</h2>
+      <ul>  
+        <li>Men</li>
+        <li>Women</li>
+        <li>Accessories</li>
+        <li>Grooming</li>
+      </ul>
     </section>
     <section class="widget-help pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
       <h2>Help</h2>
+      <ul>
+        <li>Customer Assistance</li>
+        <li>Shipping and Delivery</li>
+        <li>Returns</li>
+        <li>FAQ</li>
+      </ul>
     </section>
     <section class="widget-contact pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
       <h2>Contact <span class="light">Information</span></h2>
+      <ul>
+        <li>4012 South Surrey, BC</li>
+        <li>1C3 5F2, Canada</li>
+        <li>support@sqd.com</li>
+      </ul>
     </section>
+    <div class="sub-footer pure-u-1">
+      <p>&#169; 2017 All rights reserved by SQD</p>
+    </div>
   </footer>
 
   <?php wp_footer() ?>
